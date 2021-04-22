b264eb7cda3306973569068f3baf118082cff905
Project <nileshsinha1731@gmail.com>
2021-04-22 23:18:56 +0530

    Main Page Bottom Navigation

---
+++ app/src/main/java/com/android/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.android
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.android.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                textView.setText("Home")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                textView.setText("Search")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                textView.setText("Add Post")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                textView.setText("Notifications")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                textView.setText("Profile")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        textView=findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+}
