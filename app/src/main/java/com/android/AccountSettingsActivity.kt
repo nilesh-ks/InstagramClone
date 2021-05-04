5216bdbc0479dccdbf5ddda585b77d10003f8668
Project <nileshsinha1731@gmail.com>
2021-05-04 09:46:42 +0530

    Account Settings and Bottom Navigation

---
+++ app/src/main/java/com/android/AccountSettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.instagram.R
+
+class AccountSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_settings)
+    }
+}
--- app/src/main/java/com/android/MainActivity.kt
+++ app/src/main/java/com/android/MainActivity.kt
@@ -19,36 +19,30 @@ import com.android.Fragments.SearchFragment
 import com.android.instagram.R
 
 class MainActivity : AppCompatActivity() {
-  internal var selectedFragment: Fragment?=null
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                selectedFragment=HomeFragment()
+                moveToFragment(HomeFragment())
 
             }
             R.id.nav_search -> {
-
-                selectedFragment=SearchFragment()
+                moveToFragment(SearchFragment())
             }
             R.id.nav_add_post -> {
 
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
+                moveToFragment(NotificationsFragment())
 
-                selectedFragment=NotificationsFragment()
             }
             R.id.nav_profile -> {
+                moveToFragment(ProfileFragment())
 
-                selectedFragment=ProfileFragment()
             }
         }
-        if(selectedFragment!=null){
-            supportFragmentManager.beginTransaction().replace(
-                R.id.fragment_container,
-               selectedFragment!!
-            ).commit()
-        }
+
         false
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,10 +55,13 @@ class MainActivity : AppCompatActivity() {
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
         //To make home as default page,
-        supportFragmentManager.beginTransaction().replace(
-            R.id.fragment_container,
-            HomeFragment()
-        ).commit()
+        moveToFragment(HomeFragment())
 
+    }
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans=supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
     }
 }
