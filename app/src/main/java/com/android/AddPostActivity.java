6b3c1e0634dff5fa16264fbd924e7d264a872720
Project <nileshsinha1731@gmail.com>
2021-05-19 23:59:08 +0530

    Adding new posts functionality

---
+++ app/src/main/java/com/android/AddPostActivity.java
@@ -0,0 +1,16 @@
+package com.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.instagram.R;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
