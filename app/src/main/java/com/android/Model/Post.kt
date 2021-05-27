fd6ca04eab1a73b07a3fdb2684117ecd40d81793
Project <nileshsinha1731@gmail.com>
2021-05-27 23:58:43 +0530

    Post class initial setup

---
+++ app/src/main/java/com/android/Model/Post.kt
@@ -0,0 +1,14 @@
+package com.android.Model
+
+class Post {
+    private var postid: String=""
+    private var postimage: String=""
+    private var publisher: String=""
+    private var description: String=""
+
+
+    constructor()
+
+
+
+}
